cee8c7f4dfaae7bbcf0428b86d1b26b4e0e3cb15
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2019-08-01 02:19:27 +0600

    Permission Created

--- app/src/main/java/com/ahad/rhythm/MainActivity.java
+++ app/src/main/java/com/ahad/rhythm/MainActivity.java
@@ -1,15 +1,51 @@
 package com.ahad.rhythm;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.ahad.rhythm.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView myListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myListView = (ListView)findViewById(R.id.mySongListView);
+
+        runtimePermission();
+
     }
+
+    public void runtimePermission(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
 }
