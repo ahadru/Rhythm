4d0603a133a88630ed265399427c9df66fb9c743
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2019-08-01 03:08:19 +0600

    files read done and created a PlayerActivity

--- app/src/main/java/com/ahad/rhythm/MainActivity.java
+++ app/src/main/java/com/ahad/rhythm/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ahad.rhythm;
 
 import android.Manifest;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.ahad.rhythm.R;
@@ -13,15 +15,19 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    ListView myListView;
+    ListView myListViewForSongs;
+    String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myListView = (ListView)findViewById(R.id.mySongListView);
+        myListViewForSongs = (ListView)findViewById(R.id.mySongListView);
 
         runtimePermission();
 
@@ -33,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
-
+                        display();
                     }
 
                     @Override
@@ -46,6 +52,32 @@ public class MainActivity extends AppCompatActivity {
                         token.continuePermissionRequest();
                     }
                 }).check();
+    }
+    public ArrayList<File> findSong(File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for(File singleFile: files){
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.addAll(findSong(singleFile));
+            }
+            else{
+                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")){
+                    arrayList.add(singleFile);
+                }
+
+            }
+        }
+        return  arrayList;
+    }
+    void display() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+        for(int i = 0; i < mySongs.size(); i++){
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        myListViewForSongs.setAdapter(myAdapter);
     }
 
 }
